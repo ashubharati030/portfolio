78261a64533d5a96b0c0c166985853145aac2996
ashubharati030 <ashubharati030@gmail.com>
2020-08-09 17:15:31 +0530

    first version

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -5,9 +5,13 @@
 <br><br><br><br>
 <div style="margin-left:30px;margin-right:800px;background:rgb(201, 144, 59)" class="col-md-6  jumbotron float-left"> <!--Jumbotron controls the size of card by its margin-->
 
-    <h1><span class="badge badge-success"style="color:black">PROJECTS</span></h1>
-    <h3><b>Web Developemnt Project on creating portfolio.</b></h3>
+    <h1><span class="badge badge-success"style="color:black">PROJECTS</span></h1><br>
+    <h3><b>1)Sky-Fighter</b></h3>
+    <h4><b><u>Github link:</b></u>https://github.com/ashubharati030/SkyFighter/</h4><br>
+
+    <h3><b>2)Web Developemnt Project on creating portfolio.</b></h3>
     <h4><b><u>URL:</b></u>https://powerful-headland-26946.herokuapp.com/</h4>
+    <h4><b><u>URL:</b></u>https://github.com/ashubharati030/portfolio/</h4>
 </div>
 
 
